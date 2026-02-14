613f1c16050c7af399f39f73129c5696ff88902c
engrjake <gmotol19@gmail.com>
2026-02-14 19:56:11 +0800

    commit
    commit

---
+++ try.py
@@ -0,0 +1,11 @@
+# Simple Moment Calculator
+
+# input values
+force = float(input("Enter force (N): "))
+distance = float(input("Enter distance from pivot (m): "))
+
+# calculation
+moment = force * distance
+
+# output result
+print("Moment =", moment, "Nm")
